55a62ee81ab89ee523441b8bf54cc701a86833d2
dhs263 <dhs263@cornell.edu>
2026-02-13 13:48:57 -0500

    decode failed print has time

--- spi_shm.cpp
+++ spi_shm.cpp
@@ -357,7 +357,7 @@ private:
     std::vector<uint8_t> payload;
     if (!decode_hdlc_frame(rx, payload_len, payload)) {
       if (chipSelect == 1) { // For debugging, we only use Power
-        std::fprintf(stderr, "Failed to decode HDLC frame from CS%d\n", chipSelect);
+        std::fprintf(stderr, "Failed to decode HDLC frame from CS%d at time %d\n", chipSelect, (int)time(nullptr));
       }
       payload.clear();
     }
